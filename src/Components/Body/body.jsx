7c141040cb1a23f6bf3970e950bb10ab0ff7dbbf
123GAURAVTHADANI <garv.thadani@gmail.com>
2024-11-19 19:57:17 +0530

    Hello World

---
+++ src/Components/Body/body.jsx
@@ -0,0 +1,4 @@
+function Body() {
+  return <h1>I am PW SKills!</h1>;
+}
+export default Body;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import "./Styles/index.css";
+import App from "./Components/App/App";
+
+createRoot(document.getElementById("root")).render(<App />);
